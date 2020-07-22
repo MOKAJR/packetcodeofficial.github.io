79670ff1a8dfd3fe80baa3539502454b7012a26c
Moka jr <moka1111.mmm@gmail.com>
2020-07-22 12:06:09 -1000

    all

---
+++ new/index.php
@@ -0,0 +1,208 @@
+<!doctype html>
+
+<html lang= "en" >
+    
+    <head>
+     <meta charset="UTF-8" />
+      <!-- IE meta ----------------------------------------- -->  
+     <meta http-equiv = " X-UA-Compatible " content= "IE=edge" />
+      <!-- mobile ------------------------------------------ -->
+     <meta name= "viewport" content = " width = device-width, initial-scale = l" />
+
+        <link rel='stylesheet' href= 'css/style.css' />
+        <link rel='stylesheet' href= 'css/bootstrap.css' />
+
+        
+        <!-- ----- -->
+        <script src="js/html5shiv.min.js"></script>
+        <script src="js/respond.min.js"></script>
+        <!-- ----- -->
+     <title>bootstrap</title>
+    </head>
+    
+ <body>
+ 
+    <!-- navbar  -->
+ 
+ <nav class="navbar navbar-expand-lg navbar-expand-md navbar-expand-sm navbar-light bg-light navbar-dark bg-dark fixed-top" role="navigation" >
+  <div class="container" >
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">Moka <span>jr</span> </a>
+  <div class="collapse navbar-collapse " id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto ">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Company</a>
+      </li>        
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Services
+        </a>         
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Programing</a>
+          <a class="dropdown-item" href="#">Web Design</a>
+          <a class="dropdown-item" href="#">Desktop</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Web Hosting</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Map</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>   
+  </div>
+  </div>
+</nav>
+ 
+   <!-- end of navbar -->
+   
+   <!-- Start of Carousel -->
+   
+   <div class="bd-example">
+  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+   
+    <ol class="carousel-indicators ">
+      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active "></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
+    </ol>
+
+    
+    <div class="carousel-inner">
+        
+     <div class="carousel-item active">
+        <img src="image/1.jpg" class="d-block w-100" alt="pic 1">
+        <div class="carousel-caption d-none d-md-block">
+          <h1>Programing</h1>
+            <p class="lead" >Nulla vitae elit libero, a pharetra augue mollis interdum We love programing very much and we need larning this language in
+            this time to meak agood futuer thenks for avryone halp me in any time .
+            </p>
+             
+        </div>
+     </div>
+      
+      <div class="carousel-item">
+        <img src="image/2.jpg" class="d-block w-100" alt="pic 2">
+        <div class="carousel-caption d-none d-sm-block">
+         <h1>Web Design</h1>
+            <p class="lead" >Nulla vitae elit libero, a pharetra augue mollis interdum We love programing very much and we need larning this language in
+            this time to meak agood futuer thenks for avryone halp me in any time .
+            </p>
+        </div>
+      </div>
+      
+      <div class="carousel-item">
+        <img src="image/3.jpg" class="d-block w-100" alt="pic 3">
+        <div class="carousel-caption d-none d-sm-block">
+          <h1>Desktop</h1>
+            <p class="lead" >Nulla vitae elit libero, a pharetra augue mollis interdum We love programing very much and we need larning this language in
+            this time to meak agood futuer thenks for avryone halp me in any time .
+            </p>
+        </div>
+      </div>
+      
+      <div class="carousel-item">
+        <img src="image/4.jpg" class="d-block w-100" alt="pic 4">
+        <div class="carousel-caption d-none d-sm-block">
+          <h1>Web Hosting</h1>
+            <p class="lead" >Nulla vitae elit libero, a pharetra augue mollis interdum We love programing very much and we need larning this language in
+            this time to meak agood futuer thenks for avryone halp me in any time .
+            </p>
+        </div>
+      </div>
+      
+    </div>
+    
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+    <!-- End of Carousel -->
+    
+    
+   <!-- Start Section -->
+   
+   <section class="about text-center">
+        <div class="container" >
+          <h1> Meet Our New Template <span> Moka jr. </span> </h1>
+          
+            <p class="lead" > Hello This My New Template We Make <strong>Bootstrap</strong> Step By Step , Let Go To Begin This Broject </p>
+       </div>
+   </section>
+   
+    <!-- End Section -->
+    
+    
+    <!-- Start Section Features -->
+     
+     
+     <section class=" features text-center" >
+        <div class="container" >
+          
+            <h1>Our Features </h1>
+            <div class="row">
+                
+              <div class="col-lg-3">  
+                <h3> 100% Resposive</h3>
+                <p>this our Pragraph To use Glyphicon </p>
+              </div>
+              
+                
+              <div class="col-lg-3">  
+                <h3> 100% Resposive</h3>
+                <p>this our Pragraph To use Glyphicon </p>
+              </div>
+              
+                
+              <div class="col-lg-3">
+                
+                <h3> 100% Resposive</h3>
+                <p>this our Pragraph To use Glyphicon </p>
+              </div>
+              
+                
+              <div class="col-lg-3">  
+                <h3> 100% Resposive</h3>
+                <p>this our Pragraph To use Glyphicon </p>
+              </div>  
+                
+            </div>
+        </div>
+        
+
+     </section>
+     
+     
+     
+    <!-- End Section Features -->
+    
+    
+    
+    
+    
+        
+   
+         
+    <script src= " js/jquery-2.2.0.min.js " ></script>
+    <script src= " js/bootstrap.min.js " ></script>
+    <script src= "js/plugins.js"></script>
+   
+</body>
+</html>
